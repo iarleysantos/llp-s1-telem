a090d4bed23096e7760e1d6cf1c7dc9cf4b08efe
iarleysantos <132516935+iarleysantos@users.noreply.github.com>
2023-05-15 23:46:33 -0300

    Update matriz-somas.c

--- atividade/atividade01/matriz-somas.c
+++ atividade/atividade01/matriz-somas.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-int main () {
-   int matrix [3][3] = { {4, 7, 8}, {2, 4, 8}, {1, 3, 6} };
-   int matrix2 [3][3] = { {2, 4, 6}, {1, 2, 3}, {7, 8, 9} };
-                         
-   int i, j; 
-   for (i = 0; i < 3; i++) {
-     for (j = 0; j < 3; j++) {
-       printf (“%2d” , matrix[i][j]+matrix2[i][j]);
-       
-     }
-    printf (“\n”);
-   }
-    return 0;
- }                        
+int main() {
+  int matrix[3][3] = { {1, 3, 5}, {2, 4, 6}, {3, 5, 7} };
+  int matrix2[3][3] = { {1, 2, 3}, {3, 6, 9}, {5, 7, 8} };
+
+  int i, j;
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+      printf ("%2d ", matrix[i][j]+matrix2[i][j]);
+      
+    }
+    printf ("\n");
+  }
+  return 0;
+}
